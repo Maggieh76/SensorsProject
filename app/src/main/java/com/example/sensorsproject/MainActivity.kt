c7ba9e6b7e061b4419da0361034b95002a40faba
Margaret Hanley <tuj92824@temple.edu>
2023-12-06 13:06:26 -0500

    add bread type conditions

--- app/src/main/java/com/example/sensorsproject/MainActivity.kt
+++ app/src/main/java/com/example/sensorsproject/MainActivity.kt
@@ -18,13 +18,16 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     lateinit var sensorEventListener: SensorEventListener
     lateinit var TempSensorEventListener: SensorEventListener
-    lateinit var sensorManager : SensorManager
+    lateinit var sensorManager: SensorManager
     lateinit var spinner: Spinner
     var humiditySensor: Sensor? = null
     var temperatureSensor: Sensor? = null
     private var resume = false;
-    private lateinit var breadPick : String
-
+    private lateinit var breadPick: String
+    var optimalHumL: Int = 60
+    var optimalHumM: Int = 80
+    var optimalTempL: Int = 0
+    var optimalTempM: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,13 +68,19 @@ class MainActivity : AppCompatActivity() {
             // Apply the adapter to the spinner.
             spinner.adapter = adapter
         }
-        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                parent?.run{
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                parent?.run {
                     breadPick = getItemAtPosition(position).toString()
                     handlePick(breadPick)
                 }
             }
+
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
         }
@@ -80,8 +89,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(sensorEventListener, humiditySensor, SensorManager.SENSOR_DELAY_NORMAL)
-        sensorManager.registerListener(TempSensorEventListener, temperatureSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(
+            sensorEventListener,
+            humiditySensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+        sensorManager.registerListener(
+            TempSensorEventListener,
+            temperatureSensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
     }
 
     override fun onPause() {
@@ -89,14 +106,40 @@ class MainActivity : AppCompatActivity() {
         sensorManager.unregisterListener(sensorEventListener)
         sensorManager.unregisterListener(TempSensorEventListener)
     }
-    fun handlePick(breadPick: String){
-        when(breadPick){
-            "Sourdough" -> Log.d("Type", "Sourdough")
-            "Rye Dough" -> Log.d("Type", "Rye Dough")
-            "Sweet Dough / Croissant" -> Log.d("Type", "Sweet Dough / Croissant")
-            "Pre-ferments" -> Log.d("Type", "Pre-ferments")
-            "Other" -> Log.d("Type", "Other")
+
+    fun handlePick(breadPick: String) {
+        when (breadPick) {
+            "Sourdough" -> {
+                optimalTempL = 70
+                optimalTempM = 85
+                Log.d("Type", "Sourdough")
+            }
+            "Rye Dough" -> {
+                optimalTempL = 80
+                optimalTempM = 85
+                Log.d("Type", "Rye Dough")
+            }
+            "Sweet Dough / Croissant" -> {
+                optimalTempL = 75
+                optimalTempM = 80
+                Log.d("Type", "Sweet Dough / Croissant")
+            }
+            "Lean Dough" -> {
+                optimalTempL = 75
+                optimalTempM = 78
+                Log.d("Type", "Lean Dough")
+            }
+            "Pre-ferments" -> {
+                optimalTempL = 70
+                optimalTempM = 72
+                Log.d("Type", "Pre-ferments")
+            }
+            "Other" -> {
+                optimalTempL = 81
+                optimalTempM = 81
+                Log.d("Type", "Other")
             }
         }
 
+    }
 }
